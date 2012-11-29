7872d3f6dc64744dee30da48ed5bedb2b5ec452a
Forrest L Norvell <forrest@newrelic.com>
2012-11-28 16:26:59 -0800

    Change how the search client is bootstrapped.
    Old method used circular dependencies and was therefore brittle.

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+var path = require('path');
+
 //imports libs
+require('./lib/elasticsearchclient/calls/core');
+require('./lib/elasticsearchclient/calls/indices');
+require('./lib/elasticsearchclient/calls/cluster');
+require('./lib/elasticsearchclient/calls/twitter');
+
 module.exports = require('./lib/elasticsearchclient/elasticSearchClient');
